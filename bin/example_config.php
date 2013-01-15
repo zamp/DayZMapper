76c062702cacbe9b77e9bcd29bd48288cc424f3a
Skaronator <DayZ@Skaronator.com>
2013-01-15 16:35:24 +0100

    Example for Noobs

--- bin/example_config.php
+++ bin/example_config.php
@@ -1,10 +1,14 @@
 <?php
 $db = array();
-$db['host'] = 'localhost'; // MySQL Server Host
-$db['port'] = '3306'; // MySQL Server Port
+$db['host'] = '127.0.0.1'; // MySQL Server Host
+$db['port'] = '3306'; // MySQL Server Port (Usually no change needed)
 $db['user'] = "dayz"; // MySQL Username
 $db['password'] = "yourpassword"; // MySQL Password
 $db['database'] = "dayz"; // MySQL Database
+
+//Example for stupid noobs
+//$db['host'] = '192.168.172'; //Your Database IP Between the two '
+
 
 $config = array();
 $config['instance'] = 1; // Instance number
